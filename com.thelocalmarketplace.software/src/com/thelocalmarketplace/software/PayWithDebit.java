7e2ba2ba540cd56ee0e3df4c25e592b0c07508cc
AhmedElshabasi <ahmed@LAPTOP-5PP5RF5K>
2023-11-18 13:18:48 -0700

    Almost done with paywithdebit via swipe. About to add final touches. Will start testing.

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayWithDebit.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayWithDebit.java
@@ -1,35 +1,55 @@
 package com.thelocalmarketplace.software;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 import com.jjjwelectronics.card.Card;
 import com.jjjwelectronics.card.Card.CardData;
-import com.jjjwelectronics.card.Card.CardSwipeData;
+import com.jjjwelectronics.card.CardReaderBronze;
 import com.thelocalmarketplace.hardware.external.CardIssuer;
 
 public class PayWithDebit {
-
 	
 	
 	private boolean sysReadyForPayment = false;
-	double amountOwed;
+	BigDecimal amountOwed;
 	Card card;
-	CardSwipeData cardSwipeData;
 	CardData cardData;
 	CardIssuer cardIssuer;
 	String customerSignature;
+	CardReaderBronze cardReader;
 	
-	public void selectPaymentType() {
-		
-	}
+	 public PayWithDebit(CardIssuer issuer,BigDecimal amount) {
+	        this.cardIssuer = issuer;
+	        this.amountOwed = amount;
+	    }
+	 
 	public void sysReady() {
 		sysReadyForPayment = true;
 	}
 	
-	public void payViaSwipe(Card card) throws IOException {
+	public void promptForSignature(String signature) {
+		customerSignature = signature;
+	}
+	
+	public boolean bankAuthentication(CardData cardData) {
+		long holdNumber = cardIssuer.authorizeHold(cardData.getNumber(), amountOwed.doubleValue());
+        boolean transactionResult = cardIssuer.postTransaction(cardData.getNumber(), holdNumber, amountOwed.doubleValue());
+        cardIssuer.releaseHold(cardData.getNumber(), holdNumber);
+        return transactionResult;
+	}
+	
+public void payViaDebitSwipe(Card card, String signature) throws IOException {
+		
 		if (sysReadyForPayment==true) {
-		cardSwipeData = card.swipe();
-		cardIssuer.addCardData(cardSwipeData.getCardholder(), card.cardholder, card., customerSignature, amountOwed);}
+			System.out.println("Amount Due: "+ amountOwed);
+		cardData = cardReader.swipe(card);
+		promptForSignature(signature);
+		if (bankAuthentication(cardData)==true) {
+			System.out.println("Amount Due: 0");
+			//print receipt
+		}
+	    }
 	}
 	
 
