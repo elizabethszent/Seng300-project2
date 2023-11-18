72c195c0e14c4a9459ca8d7ab7355cd61017aa94
JaneBM <janebmagai@gmail.com>
2023-11-17 18:51:41 -0700

    "Updates on PayViaBanknote"

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayViaBanknote.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayViaBanknote.java
@@ -1,23 +1,20 @@
 package com.thelocalmarketplace.software;
-
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Currency;
 import java.util.List;
-
-import com.jjjwelectronics.IDevice;
-import com.jjjwelectronics.IDeviceListener;
+import java.util.Map;
 import com.tdc.CashOverloadException;
 import com.tdc.DisabledException;
 import com.tdc.IComponent;
 import com.tdc.IComponentObserver;
+import com.tdc.NoCashAvailableException;
+import com.tdc.banknote.AbstractBanknoteDispenser;
 import com.tdc.banknote.Banknote;
 import com.tdc.banknote.BanknoteDispensationSlot;
 import com.tdc.banknote.BanknoteDispensationSlotObserver;
+import com.tdc.banknote.BanknoteInsertionSlot;
+import com.tdc.banknote.BanknoteInsertionSlotObserver;
 import com.tdc.banknote.BanknoteStorageUnit;
 import com.tdc.banknote.BanknoteStorageUnitObserver;
-import com.tdc.banknote.BanknoteValidator;
-import com.tdc.banknote.BanknoteValidatorObserver;
 
 /**
  * PayViaBanknote class handles payments through the use of banknotes, monitors payment process and 
@@ -26,51 +23,72 @@ import com.tdc.banknote.BanknoteValidatorObserver;
  * @author Jane Magai (UCID:30180119)
  * 
  */
-public class PayViaBanknote implements BanknoteStorageUnitObserver,BanknoteDispensationSlotObserver,BanknoteValidatorObserver {
-	private BigDecimal amount_inserted = BigDecimal.ZERO;
-	private BigDecimal amount_owed;
-	private BanknoteDispensationSlot banknoteDispensationSlot;
-	private List <Banknote> banknotes;
-		
+public class PayViaBanknote implements BanknoteStorageUnitObserver,BanknoteDispensationSlotObserver,BanknoteInsertionSlotObserver {
+	private BigDecimal amountInserted;
+	private BigDecimal amountOwed;
+	private BanknoteDispensationSlot dispensationSlot;
+	private BanknoteInsertionSlot insertionSlot;
+	private List<java.util.Map.Entry < BigDecimal,AbstractBanknoteDispenser>> dispenserList;
+	private BigDecimal change;
+		 
 	
 	/**
-	 * Constructor for PayviaCoin class.
-	 * @param totalAmount 
+	 * Constructor for PayviaBanknote class.
+	 * 
+	 * @param amountOwed      The amount owed for payment
+	 * @param dispenserSlot   Represents BanknoteDispensationslot that dispenses banknotes.
+	 * @param insertSlot      Represents BanknoteInsertionslot where banknotes are entered.
+	 * @param dispenser       Abstract version of BanknoteDispensor to allow use of all tiers of checkout system (Bronze, Silver, Gold)
+	 * 
 	 */
-	public PayViaBanknote() {
-		this.amount_inserted = BigDecimal.ZERO;
-		this.amount_owed = BigDecimal.ZERO;
-		this.banknoteDispensationSlot = ;
-		this.banknotes = new ArrayList<>();
-		
+	
+	public PayViaBanknote(BigDecimal amountOwed, BanknoteDispensationSlot dispensationSlot, BanknoteInsertionSlot insertionSlot, List<java.util.Map.Entry < BigDecimal,AbstractBanknoteDispenser>> dispenserList) {
+		this.amountOwed = amountOwed;
+		this.dispensationSlot = dispensationSlot;
+		this.insertionSlot = insertionSlot;
+		this.dispenserList = dispenserList ;
 	}
-	
-	public void addBanknote(Banknote banknote) {
 		
-		
-	}
-	
-	
 	
 	/**
-	 *Make a payment using banknote
+	 *Shows making a payment using banknote
 	 *
 	 *@param amountInserted. The banknote used for payment.
 	 *@return True if payment is succesful meaning full amount is paid, false otherwise
 	 * @throws CashOverloadException 
 	 * @throws DisabledException 
 	 */
-	public boolean MakePayment (Banknote amountInserted) throws DisabledException, CashOverloadException{
-		amount_inserted = amount_inserted.add(amountInserted.getDenomination());
-		banknoteDispensationSlot.receive(amountInserted);
-		if(amount_inserted.compareTo(amount_owed)<=0) {
-			return true;
-		}
-		return false;
+	public boolean makePayment (Banknote banknoteAdded){
+		amountInserted = amountInserted.add(banknoteAdded.getDenomination());
+	    if(amountInserted.compareTo(amountOwed)>=0) {
+	    	change = amountInserted.subtract(amountOwed);
+	    	return true;	    	
+	    }
+	    else
+	    	return false;	        
 	}
-	public boolean ReturnChange() {
+	
+	/**
+	 * Returns change to customer, does not return chsnge if change filed is zero.
+	 */			
 		
-		
+	public void returnChange() throws CashOverloadException, DisabledException{
+		for (Map.Entry<BigDecimal, AbstractBanknoteDispenser> entry : dispenserList) {
+	        BigDecimal exactChange = entry.getKey();
+	        AbstractBanknoteDispenser banknoteDispenser = entry.getValue();
+
+	        BigDecimal requiredBanknotes = change.divideToIntegralValue(exactChange);
+
+	        for (int i = 0; i < requiredBanknotes.intValue(); i++) {
+	            try {
+	                banknoteDispenser.emit();
+	                change = change.subtract(exactChange);
+	            } catch (NoCashAvailableException e) {
+	      
+	                break;
+	            }
+	            }
+	        }
 		
 	}
 	
@@ -134,13 +152,22 @@ public class PayViaBanknote implements BanknoteStorageUnitObserver,BanknoteDispe
 		// TODO Auto-generated method stub
 		
 	}
+	
+
 	@Override
-	public void goodBanknote(BanknoteValidator validator, Currency currency, BigDecimal denomination) {
+	public void banknoteInserted(BanknoteInsertionSlot slot) {
 		// TODO Auto-generated method stub
 		
 	}
+
 	@Override
-	public void badBanknote(BanknoteValidator validator) {
+	public void banknoteEjected(BanknoteInsertionSlot slot) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void banknoteRemoved(BanknoteInsertionSlot slot) {
 		// TODO Auto-generated method stub
 		
 	}
