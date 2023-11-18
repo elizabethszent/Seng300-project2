186fa1139f666a48365082657fb0da22964e9f18
AhmedElshabasi <ahmed@LAPTOP-5PP5RF5K>
2023-11-17 17:00:00 -0700

    Added payviaswipe method and sysReady, and necessary variables.

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayWithDebit.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayWithDebit.java
@@ -1,5 +1,36 @@
 package com.thelocalmarketplace.software;
 
+import java.io.IOException;
+
+import com.jjjwelectronics.card.Card;
+import com.jjjwelectronics.card.Card.CardData;
+import com.jjjwelectronics.card.Card.CardSwipeData;
+import com.thelocalmarketplace.hardware.external.CardIssuer;
+
 public class PayWithDebit {
+
+	
+	
+	private boolean sysReadyForPayment = false;
+	double amountOwed;
+	Card card;
+	CardSwipeData cardSwipeData;
+	CardData cardData;
+	CardIssuer cardIssuer;
+	String customerSignature;
+	
+	public void selectPaymentType() {
+		
+	}
+	public void sysReady() {
+		sysReadyForPayment = true;
+	}
+	
+	public void payViaSwipe(Card card) throws IOException {
+		if (sysReadyForPayment==true) {
+		cardSwipeData = card.swipe();
+		cardIssuer.addCardData(cardSwipeData.getCardholder(), card.cardholder, card., customerSignature, amountOwed);}
+	}
+	
 
 }
