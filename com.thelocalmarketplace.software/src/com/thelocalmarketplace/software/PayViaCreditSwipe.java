1d6f137018e9bca2c0076c4d63f0938716e9a31c
Jaden Myers <jadenmyers99@gmail.com>
2023-11-19 15:15:09 -0700

    Updated PayViaCreditSwipe

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayViaCreditSwipe.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayViaCreditSwipe.java
@@ -1,17 +1,29 @@
 package com.thelocalmarketplace.software;
 
+import com.thelocalmarketplace.hardware.AbstractSelfCheckoutStation;
 import com.thelocalmarketplace.hardware.external.*;
+import com.jjjwelectronics.IDevice;
+import com.jjjwelectronics.IDeviceListener;
+import com.jjjwelectronics.card.*;
+import com.jjjwelectronics.card.Card.CardData;
+
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 /**
  * Class to handle payments made by swiping a credit card.
  *
  */
-public class PayViaCreditSwipe {
+public class PayViaCreditSwipe implements CardReaderListener {
     CardIssuer issuer;
-    public PayViaCreditSwipe(CardIssuer issuer) {
+    AbstractSelfCheckoutStation station;
+    ArrayList<String> creditTypes = new ArrayList<String>(Arrays.asList("Visa", "Mastercard"));
+    
+    public PayViaCreditSwipe(CardIssuer issuer, AbstractSelfCheckoutStation station) {
         this.issuer = issuer;
+        this.station = station;
     }
 
     /**
@@ -19,10 +31,45 @@ public class PayViaCreditSwipe {
      * Communicates details of payment and card information to the bank.
      *	@return true if the credit card payment was successful, false otherwise
      */
-    public boolean creditCardScanned(String cardNumber, BigDecimal amountDue) {
-        long holdNumber = issuer.authorizeHold(cardNumber, amountDue.doubleValue());
-        boolean success = issuer.postTransaction(cardNumber, holdNumber, amountDue.doubleValue());
-        issuer.releaseHold(cardNumber, holdNumber);
-        return success;
+    public void theDataFromACardHasBeenRead(CardData data) {
+    	if (creditTypes.contains(data.getType())) {
+    		String cardNumber = data.getNumber();
+    		
+    		long holdNumber = issuer.authorizeHold(cardNumber, amountDue.doubleValue());
+            boolean success = issuer.postTransaction(cardNumber, holdNumber, amountDue.doubleValue());
+            issuer.releaseHold(cardNumber, holdNumber);
+            if (success)
+                System.out.println("New amount due: 0");
+    	}
     }
+
+	@Override
+	public void aDeviceHasBeenEnabled(IDevice<? extends IDeviceListener> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void aDeviceHasBeenDisabled(IDevice<? extends IDeviceListener> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void aDeviceHasBeenTurnedOn(IDevice<? extends IDeviceListener> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void aDeviceHasBeenTurnedOff(IDevice<? extends IDeviceListener> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void aCardHasBeenSwiped() {
+		// TODO Auto-generated method stub
+		
+	}
 }
