ef4cf86938bd3ee51e7a8274f01ae05c7a403b5b
AhmedElshabasi <ahmed@LAPTOP-5PP5RF5K>
2023-11-19 01:00:22 -0700

    Halfway done with the tests. Added some getters and made slight modifications that were needed after testing.

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/PayWithDebitTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/PayWithDebitTest.java
@@ -1,5 +1,7 @@
 package com.thelocalmarketplace.software.test;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
 import java.math.BigDecimal;
 
@@ -8,49 +10,64 @@ import org.junit.Test;
 
 import com.jjjwelectronics.card.Card;
 import com.jjjwelectronics.card.Card.CardData;
+import com.jjjwelectronics.card.Card.CardSwipeData;
 import com.thelocalmarketplace.hardware.external.CardIssuer;
 import com.thelocalmarketplace.software.PayWithDebit;
 
 public class PayWithDebitTest {
-
+	CardIssuer cardIssuer;
+	BigDecimal amountOwed;
+    PayWithDebit payWithDebit;
+    Card card;
+    Card.CardSwipeData cardData;
 	
+  
 	@Before
     public void setUp() {
-    	 /**
-         * Sets up the test by creating an instance of the StartSession class.
-         */
-		CardIssuer cardIssuer = new CardIssuer("SomeName", 4);
-		BigDecimal amountOwed = new BigDecimal(20);
-        PayWithDebit payWithDebit = new PayWithDebit(cardIssuer,amountOwed);
+    	
+		 cardIssuer = new CardIssuer("SomeName", 4);
+		 amountOwed = new BigDecimal(20);
+         payWithDebit = new PayWithDebit(cardIssuer,amountOwed);
+         card = new Card("Visa",  "1234567890123456", "SomeName", "111");
+         cardData = card.new CardSwipeData();
+         
     }
 	
 	@Test
 	public void testPayWithDebitConstructor() {
-        
+//		CardIssuer issuer = new CardIssuer("SomeName", 4);
+//		BigDecimal owed = new BigDecimal(20);
+//		 assertEquals(issuer. ),payWithDebit.getCardIssuer().toString());
+//		 assertEquals(owed.toString(),payWithDebit.getAmountOwed().toString());
     }
  
 	@Test
 	public void testSysReady() {
+	payWithDebit.sysReady();
+	boolean expected = true;
+	assertEquals(expected,payWithDebit.isSysReadyForPayment());
+}
+
+	@Test
+	public void testpromptForSignature() {
+	String signature = "signature";
+	payWithDebit.promptForSignature(signature);
+	assertEquals(signature,payWithDebit.getCustomerSignature());
+}
+	@Test
+
+	public void testBankAuthentication() {
+	payWithDebit.bankAuthentication(cardData);
 	
 }
 
 	@Test
-	public void promptForSignature(String signature) {
-	
-}
-	@Test
-
-	public void testBankAuthentication(CardData cardData) {
-	
-}
-
-	@Test
-	public void testPayViaDebitSwipe(Card card, String signature) throws IOException {
+	public void testPayViaDebitSwipe() throws IOException {
 	
 	
 }
 	@Test
-	public void testPayViaDebitSwipeListener(Card card, String signature) throws IOException {
+	public void testPayViaDebitSwipeListener() throws IOException {
 	
 	
 }
--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayWithDebit.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayWithDebit.java
@@ -5,34 +5,40 @@ import java.math.BigDecimal;
 
 import com.jjjwelectronics.card.Card;
 import com.jjjwelectronics.card.Card.CardData;
+import com.jjjwelectronics.card.Card.CardSwipeData;
 import com.jjjwelectronics.card.CardReaderBronze;
 import com.thelocalmarketplace.hardware.external.CardIssuer;
 
 public class PayWithDebit {
 	
 	
-	private boolean sysReadyForPayment = false;
+	
+	boolean sysReadyForPayment = false;
+	
+
 	BigDecimal amountOwed;
 	Card card;
-	CardData cardData;
+	CardSwipeData cardData;
 	CardIssuer cardIssuer;
 	String customerSignature;
 	CardReaderBronze cardReader;
+
 	
 	 public PayWithDebit(CardIssuer issuer,BigDecimal amount) {
 	        this.cardIssuer = issuer;
 	        this.amountOwed = amount;
 	    }
 	 
-	public void sysReady() {
-		sysReadyForPayment = true;
-	}
 	
+	public boolean isSysReadyForPayment() {
+		return sysReadyForPayment;
+	}
+
 	public void promptForSignature(String signature) {
 		customerSignature = signature;
 	}
 	
-	public boolean bankAuthentication(CardData cardData) {
+	public boolean bankAuthentication(CardSwipeData cardData) {
 		long holdNumber = cardIssuer.authorizeHold(cardData.getNumber(), amountOwed.doubleValue());
         boolean transactionResult = cardIssuer.postTransaction(cardData.getNumber(), holdNumber, amountOwed.doubleValue());
         cardIssuer.releaseHold(cardData.getNumber(), holdNumber);
@@ -43,7 +49,7 @@ public void payViaDebitSwipe(Card card, String signature) throws IOException {
 		
 		if (sysReadyForPayment==true) {
 			System.out.println("Amount Due: "+ amountOwed);
-		cardData = cardReader.swipe(card);
+		cardData = (CardSwipeData) cardReader.swipe(card);
 		promptForSignature(signature);
 		if (bankAuthentication(cardData)==true) {
 			System.out.println("Amount Due: 0");
@@ -51,6 +57,22 @@ public void payViaDebitSwipe(Card card, String signature) throws IOException {
 		}
 	    }
 	}
-	
+
+public CardIssuer getCardIssuer() {
+	return cardIssuer;
+}
+
+public BigDecimal getAmountOwed() {
+	return amountOwed;
+}
+
+public void sysReady() {
+	sysReadyForPayment = true;
+}
+
+
+public String getCustomerSignature() {
+	return customerSignature;
+}
 
 }
