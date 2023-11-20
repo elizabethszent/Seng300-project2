e5df2169a4d0941e39245b265911e0a20c577e98
AhmedElshabasi <ahmed.elshabasi@ucalgary.ca>
2023-11-20 07:24:53 -0700

    Finished the PayWithDebit Class and all the testing. All tests pass.

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/PayWithDebitTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/PayWithDebitTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertEquals;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Calendar;
-import java.util.Currency;
 
 import org.junit.After;
 import org.junit.Before;
@@ -15,12 +14,9 @@ import com.jjjwelectronics.IDevice;
 import com.jjjwelectronics.IDeviceListener;
 import com.jjjwelectronics.card.Card;
 import com.jjjwelectronics.card.Card.CardData;
-import com.jjjwelectronics.card.Card.CardSwipeData;
 import com.jjjwelectronics.card.CardReaderBronze;
 import com.jjjwelectronics.card.CardReaderGold;
 import com.jjjwelectronics.card.CardReaderListener;
-import com.tdc.coin.CoinSlot;
-import com.thelocalmarketplace.hardware.SelfCheckoutStationBronze;
 import com.thelocalmarketplace.hardware.external.CardIssuer;
 import com.thelocalmarketplace.software.PayWithDebit;
 
@@ -30,8 +26,11 @@ public class PayWithDebitTest {
 	CardIssuer cardIssuer;
 	BigDecimal amountOwed;
     PayWithDebit payWithDebit;
+    PayWithDebit payWithDebit2;
     Card card;
     Card.CardSwipeData cardData;
+    Card card2;
+    Card.CardSwipeData cardData2;
     CardReaderBronze cardReader;
     CardReaderGold cardReaderGold;
     private static int swipeCount;
@@ -45,8 +44,11 @@ public class PayWithDebitTest {
 		 cardIssuer = new CardIssuer("SomeName", 4);
 		 amountOwed = new BigDecimal(20);
          payWithDebit = new PayWithDebit(cardIssuer,amountOwed);
+         payWithDebit2 = new PayWithDebit(cardIssuer,amountOwed);
          card = new Card("Visa",  "1234567890123456", "SomeName", "111");
          cardData = card.new CardSwipeData();
+         card2 = new Card("Visa",  "1234567890654321", "SomeOtherName", "111");
+         cardData2 = card2.new CardSwipeData();
          PowerGrid power = PowerGrid.instance();
          cardReader = new CardReaderBronze();
          cardReader.plugIn(power);
@@ -66,6 +68,8 @@ public class PayWithDebitTest {
 		 dataHasBeenRead = false;
 	}
  
+	/*Tests to see if the method that makes sure the system is ready is working
+	 * and that the variable responsible for this is changing accordingly*/
 	@Test
 	public void testSysReady() {
 		payWithDebit.sysReady();
@@ -73,14 +77,18 @@ public class PayWithDebitTest {
 		assertEquals(expected,payWithDebit.isSysReadyForPayment());
 }
 
+	/*Tests the promptForSignature method, the method that asks for 
+	 * customer's signature*/
 	@Test
 	public void testpromptForSignature() {
 		String signature = "signature";
 		payWithDebit.promptForSignature(signature);
 		assertEquals(signature,payWithDebit.getCustomerSignature());
 }
+	/* Tests the bankAuthentication method, the method that is responsible for making 
+	sure that a transaction occurs correctly by taking a card that operates correctly
+	as a parameter*/
 	@Test
-
 	public void testBankAuthentication() {
 		boolean expected = true;
 		boolean actual = payWithDebit.bankAuthentication(cardData);
@@ -88,12 +96,54 @@ public class PayWithDebitTest {
 	
 }
 	
+	/*Tests the bankAuthentication method to see if it acts correctly if transaction fails 
+	 * by taking a card that doesn't exist in the database it takes a card that */
 	@Test
-	
-	public void testCardBlockedAfterAuthentication() {
+	public void testBankAuthenticationFail() {
+		boolean expected = false;
+		boolean actual = payWithDebit2.bankAuthentication(cardData2);
+		assertEquals(expected,actual);
 		
 	}
+	
+	/*
+	 * Tests how payViaDebitSwipe would react if bank transaction doesn't work because card is not in card issuer database
+	 */
+	@Test
+	public void testCardBlockedAfterFailedAttempts() throws IOException {
+		int expectedFailedAttempts = 4;
+		boolean expectedCardBlocked = true;
+		payWithDebit.sysReady();
+		payWithDebit.payViaDebitSwipe(card2, "signature",cardReader);
+		payWithDebit.payViaDebitSwipe(card2, "signature",cardReader);
+		payWithDebit.payViaDebitSwipe(card2, "signature",cardReader);
+		payWithDebit.payViaDebitSwipe(card2, "signature",cardReader);
+		assertEquals(expectedFailedAttempts,payWithDebit.getFailedAttempts());
+		assertEquals(expectedCardBlocked,payWithDebit.isCardBlocked());
+	}
+	
+	/*
+	 * Tests how payViaDebitSwipe would react if the amount owed is greater than the money available on the debit card
+	 */
+	@Test
+	public void testCardBlockedWhenAmountOwedHigher() throws IOException {
+		Calendar expiryDate = new Calendar.Builder().setDate(2028, Calendar.SEPTEMBER, 1).build();
+		cardIssuer.addCardData("1234567890654321", "SomeOtherName", expiryDate, "111",  1000); 
+		payWithDebit2 = new PayWithDebit(cardIssuer,new BigDecimal(1100));
+		
+		int expectedFailedAttempts = 4;
+		boolean expectedCardBlocked = true;
+		payWithDebit2.sysReady();
+		payWithDebit2.payViaDebitSwipe(card2, "signature",cardReader);
+		payWithDebit2.payViaDebitSwipe(card2, "signature",cardReader);
+		payWithDebit2.payViaDebitSwipe(card2, "signature",cardReader);
+		payWithDebit2.payViaDebitSwipe(card2, "signature",cardReader);
+		assertEquals(expectedFailedAttempts,payWithDebit2.getFailedAttempts());
+		assertEquals(expectedCardBlocked,payWithDebit2.isCardBlocked());
+	}
 
+	/*Tests to ensure the method PayViaDebitSwipe is working correctly by comparing 
+	 * the new amount owed of 0 after a successful transaction to the expected*/
 	@Test
 	public void testPayViaDebitSwipe() throws IOException {
 		BigDecimal expectedAmount = new BigDecimal(0);
@@ -103,12 +153,13 @@ public class PayWithDebitTest {
 	
 	
 }
+	/*Tests the listeners that should get called once the method PayViaDebitSwipe runs*/
 	@Test
 	public void testPayViaDebitSwipeListeners() throws IOException {
 		BigDecimal expectedAmount = new BigDecimal(0);
 		payWithDebit.sysReady();
-		cardReader.register(new StubCardReader());
-		payWithDebit.payViaDebitSwipe(card, "signature",cardReader);
+		cardReaderGold.register(new StubCardReader());
+		payWithDebit.payViaDebitSwipe(card, "signature",cardReaderGold);
 		assertEquals(expectedAmount,payWithDebit.getAmountOwed());
 		assertEquals(1,swipeCount);
 		assertEquals(true,dataHasBeenRead);
@@ -116,7 +167,9 @@ public class PayWithDebitTest {
 }
 	
 	
-	
+	/*
+	 * Stub class that helps in implementing testing for listeners
+	 */
 	static class StubCardReader implements CardReaderListener{
 
 		@Override
--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayWithDebit.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayWithDebit.java
@@ -9,7 +9,6 @@ import com.jjjwelectronics.card.AbstractCardReader;
 import com.jjjwelectronics.card.Card;
 import com.jjjwelectronics.card.Card.CardData;
 import com.jjjwelectronics.card.Card.CardSwipeData;
-import com.jjjwelectronics.card.CardReaderBronze;
 import com.jjjwelectronics.card.CardReaderListener;
 import com.thelocalmarketplace.hardware.external.CardIssuer;
 
@@ -21,33 +20,48 @@ public class PayWithDebit implements CardReaderListener{
 	
 
 	private BigDecimal amountOwed;
-	private Card card;
 	private CardSwipeData cardData;
 	private CardIssuer cardIssuer;
 	private String customerSignature;
 	private long holdNumber;
-	private int failedAttempts =0; 
+	private int failedAttempts =0;
+	private boolean cardBlocked = true;
 
-	
+	/*
+	 * Constructor for PayWithDebit
+	 * @param issuer
+	 * 			The info of the card issuer
+	 * @param amount
+	 * 			The amount owed by the customer 
+	 */
 	 public PayWithDebit(CardIssuer issuer,BigDecimal amount) {
 	        this.cardIssuer = issuer;
 	        this.amountOwed = amount;
 	    }
 	 
+	 /*
+	  * Method responsible for making the system ready for payment
+	  */
 	 public void sysReady() {
 			sysReadyForPayment = true;
 		}
 
+	 /*
+	  * Prompts the customer for a signature
+	  */
 	public void promptForSignature(String signature) {
 		customerSignature = signature;
 	}
 	
+	/*
+	 * Authenticates the transaction after verifying with the bank
+	 */
 	public boolean bankAuthentication(CardSwipeData cardData) {
 		holdNumber = cardIssuer.authorizeHold(cardData.getNumber(), amountOwed.doubleValue());
         boolean transactionResult = cardIssuer.postTransaction(cardData.getNumber(), holdNumber, amountOwed.doubleValue());
         cardIssuer.releaseHold(cardData.getNumber(), holdNumber);
         if(transactionResult==false) {
-        	failedAttempts++;
+        	failedAttempts+=1;
         }
         if(failedAttempts>3) {
         	transactionResult=false;
@@ -55,77 +69,104 @@ public class PayWithDebit implements CardReaderListener{
         return transactionResult;
 	}
 	
-public void payViaDebitSwipe(Card card, String signature,AbstractCardReader cardReader) throws IOException {
-		
-		if (sysReadyForPayment==true && failedAttempts<=3) {
-			System.out.println("Amount Due: "+ amountOwed);
-		boolean swipeExecuted = false;
-		while(swipeExecuted==false) {
-			try{cardData = (CardSwipeData) cardReader.swipe(card);
-			swipeExecuted=true;
-		
-		promptForSignature(signature);
-		if (bankAuthentication(cardData)==true) {
-			amountOwed = new BigDecimal(0);
-			System.out.println("Amount Due: " + amountOwed);
-			
-		}
-		else {
-			System.out.println("Transaction Failed please try again.");
-		}
-	    }
-		catch(Exception e) {}}	
+	/*
+	 * Method pays with debit using the action of swiping the card on the card reader
+	 */
+	public void payViaDebitSwipe(Card card, String signature,AbstractCardReader cardReader) throws IOException {
+			if (sysReadyForPayment==true) {
+				if(failedAttempts<3) {
+					System.out.println("Amount Due: "+ amountOwed);
+					boolean swipeExecuted = false;
+				
+					while(swipeExecuted==false) {
+						try{cardData = (CardSwipeData) cardReader.swipe(card);
+							swipeExecuted=true;
+					
+							promptForSignature(signature);
+							if (bankAuthentication(cardData)==true) {
+								amountOwed = new BigDecimal(0);
+								System.out.println("Amount Due: " + amountOwed);
+						         }
+							else {if(failedAttempts<3){
+								System.out.println("Transaction failed. Please try again.");
+							}
+								  else if(failedAttempts==3){
+										System.out.println("Maximum amount of tries to reached.");
+										cardBlocked = true;
+							}}
+						    }	
+						catch(Exception e) {}
+							}	
+			}
+				
+				else{
+					System.out.println("Card blocked.");
+					failedAttempts+=1;
+				}}
+			if(amountOwed.doubleValue()== new BigDecimal(0).doubleValue()) {
+				sysReadyForPayment = false;
+			}}
+	/*
+	 * Getter methods
+	 */
+	
+	
+	public boolean isSysReadyForPayment() {
+		return sysReadyForPayment;
 	}
-		else{
-			System.out.println("Card blocked.");
-		}}
-
-
-public boolean isSysReadyForPayment() {
-	return sysReadyForPayment;
-}
-
-public CardIssuer getCardIssuer() {
-	return cardIssuer;
-}
-
-public BigDecimal getAmountOwed() {
-	return amountOwed;
-}
-
-
-public String getCustomerSignature() {
-	return customerSignature;
-}
-
-@Override
-public void aDeviceHasBeenEnabled(IDevice<? extends IDeviceListener> device) { 
 	
-}
-
-@Override
-public void aDeviceHasBeenDisabled(IDevice<? extends IDeviceListener> device) {
+	public CardIssuer getCardIssuer() {
+		return cardIssuer;
+	}
 	
-}
-
-@Override
-public void aDeviceHasBeenTurnedOn(IDevice<? extends IDeviceListener> device) {
-	 
-}
-
-@Override
-public void aDeviceHasBeenTurnedOff(IDevice<? extends IDeviceListener> device) {
-	 
-}
-
-@Override
-public void aCardHasBeenSwiped() {
-	 
-}
-
-@Override
-public void theDataFromACardHasBeenRead(CardData data) {
+	public BigDecimal getAmountOwed() {
+		return amountOwed;
+	}
 	
-}
-
-}
+	
+	public String getCustomerSignature() {
+		return customerSignature;
+	}
+	
+	public int getFailedAttempts() {
+		return failedAttempts;
+	}
+	
+	public boolean isCardBlocked() {
+		return cardBlocked;
+	}
+	
+	
+	
+	
+	@Override
+	public void aDeviceHasBeenEnabled(IDevice<? extends IDeviceListener> device) { 
+		
+	}
+	
+	@Override
+	public void aDeviceHasBeenDisabled(IDevice<? extends IDeviceListener> device) {
+		
+	}
+	
+	@Override
+	public void aDeviceHasBeenTurnedOn(IDevice<? extends IDeviceListener> device) {
+		 
+	}
+	
+	@Override
+	public void aDeviceHasBeenTurnedOff(IDevice<? extends IDeviceListener> device) {
+		 
+	}
+	
+	@Override
+	public void aCardHasBeenSwiped() {
+		 
+	}
+	
+	@Override
+	public void theDataFromACardHasBeenRead(CardData data) {
+		
+	}
+	
+	}
