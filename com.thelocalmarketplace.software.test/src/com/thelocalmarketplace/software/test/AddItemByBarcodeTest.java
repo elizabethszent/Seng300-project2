ced7bb31c23398105b57f823de7084e0410eb96a
Arthur-Huan <c.arthurh@outlook.com>
2023-11-20 22:24:25 -0700

    Add names and UCIDs

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/AddItemByBarcodeTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/AddItemByBarcodeTest.java
@@ -1,8 +1,15 @@
-//Elizabeth Szentmiklossy UCID: 30165216
-//Justine Mangaliman UCID: 30164741
-//Enzo Mutiso UCID: 30182555
-//Abdelrahman Mohamed UCID: 30162037
-//Mohammad Mustafa Mehtab UCID: 30189394
+/**
+* Jon Mulyk (UCID: 30093143)
+* Elizabeth Szentmiklossy (UCID: 30165216)
+* Ahmed Ibrahim Mohamed Seifledin Hadsan (UCID: 30174024)
+* Arthur Huan (UCID: 30197354)
+* Jaden Myers (UCID: 30152504)
+* Jane Magai (UCID: 30180119)
+* Ahmed Elshabasi (UCID: 30188386)
+* Jincheng Li (UCID: 30172907)
+* Sina Salahshour (UCID: 30177165)
+* Anthony Tolentino (UCID: 30081427) */
+
 
 package com.thelocalmarketplace.software.test;
 
